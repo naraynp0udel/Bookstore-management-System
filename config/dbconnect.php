558c018018d976256be54262aaa449b04288d88d
Maniraj Gautam <Lucifermaniraj@gmail.com>
2020-10-28 11:22:38 +0545

    added mysql connection and little bit css on navbar

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,5 +1,13 @@
 <?php
-server="localhost"
+$host="localhost";
+$database="bookstore";
+$dbusername="root";
+$dbpassword="";
+$dsn="mysql:host=$host;dbname=$database;charset=utf8";
+$pdo= new PDO($dsn,$dbusername,$dbpassword); #Connection for Mysql
 
+$query = "INSERT INTO registered_users(Name, Email, Password) VALUES ('Maniraj','manirajg9@gmail.com','test2212')";
+$query_success=$pdo->query($query);
+#Inserting the data to the registerd users table
 
 ?>
