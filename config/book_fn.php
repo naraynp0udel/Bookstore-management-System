afa4a7c3c0e3a13007adf493baef507ecd5960a7
Maniraj Gautam <Lucifermaniraj@gmail.com>
2021-02-17 19:59:40 +0545

    file upload added and backend modification

--- config/book_fn.php
+++ config/book_fn.php
@@ -16,15 +16,31 @@ if(isset($_POST['List_Book']))
 function add_book()
 {
     $con=connectdb();
-
+    if(isset($_POST['Add_Book']))
+{
     $Book_name=strip_tags(trim($_POST['Book_name']," "));
     $Author_name=strip_tags(trim($_POST['Author_name'],""));
     $Publication=strip_tags(trim($_POST['Publication'],""));
     $Edition=strip_tags(trim($_POST['Edition'],""));
     $ISBN=strip_tags(trim($_POST["ISBN"]," "));
     $Stock=strip_tags(trim($_POST["Stock"]," "));
+
+    $dir="../upload/";
+    $image=$_FILES['uploadfile']['name'];
+    $temp_name=$_FILES['uploadfile']['tmp_name'];
+ 
+    if($image!="")
+    {
+        if(file_exists($dir.$image))
+        {
+            $image= time().'_'.$image;
+        }
+ 
+        $fdir= $dir.$image;
+        move_uploaded_file($temp_name, $fdir);
+    }
   
-        $query = "INSERT INTO books(`Book Name`,`Author Name`,`Edition`,`Publication`,`ISBN Number`,`Stock`) VALUES (?,?,?,?,?,?)";
+        $query = "INSERT INTO books(`Book Name`,`Author Name`,`Edition`,`Publication`,`ISBN Number`,`Stock`,`image`) VALUES (?,?,?,?,?,?,?)";
     
         $stmt=$con->prepare($query);
         $stmt->bindparam(1,$Book_name);
@@ -33,28 +49,38 @@ function add_book()
         $stmt->bindparam(4,$Publication);
         $stmt->bindparam(5,$ISBN);
         $stmt->bindparam(6,$Stock);
+        $stmt->bindparam(7,$image);
         $stmt->execute();
 
         
-
+}
     
 }
 function show_list_of_books()
 {
     $con=connectdb();
+    if(isset($_POST['List_Book']))
+    {
     $Book_name=strip_tags(trim($_POST['Book_name']," "));
     $Author_name=strip_tags(trim($_POST['Author_name'],""));
     $Publication=strip_tags(trim($_POST['Publication'],""));
     $Edition=strip_tags(trim($_POST['Edition'],""));
     $ISBN=strip_tags(trim($_POST["ISBN"]," "));
     
-    if(empty($Book_name&$Author_name&$Publication&$Edition&$ISBN))
+    if($Book_name&$Author_name)#$Book_name&$Author_name&$Publication&$Edition&$ISBN
     {
-        $query="select * from books";
+        $query="select `Book Name` from books where ?=? or ?=?";
         $stmt=$con->prepare($query);
-        $result=$stmt->execute();
-        
-        print_r(var_dump($result));
+        $stmt->bindparam(1,$Book_name);
+        $stmt->bindparam(2,$Book_name);
+        $stmt->bindparam(3,$Author_name);
+        $stmt->bindparam(4,$Author_name);
+        $stmt->execute();
+        $results=$stmt->fetchAll();
+        foreach ($results as $result)
+        {
+        print($result[0]);
+        }
     }
     elseif ($ISBN)
     {
@@ -63,9 +89,14 @@ function show_list_of_books()
         $stmt=$con->prepare($query);
         $stmt->bindparam(1,$param);
         $stmt->bindparam(2,$ISBN);
-        $result=$stmt->execute();
-        print_r(var_dump($result));
+        $stmt->execute();
+        $results=$stmt->fetchAll();
+        foreach ($results as $result)
+        {
+        print($result[2]);
+        }
     }
+}
 }
 
 function update_book()
--- listbook.php
+++ listbook.php
@@ -9,22 +9,7 @@ include_once 'config/auth.php'; ?>
     </title>
 </head>
 <body>
-    <form class="add-book" method="POST" action="config/book_fn.php">
-        <label for="Book_name"> Book Name </label>
-            <input type="text" name="Book_name" id="Book_name" placeholder="Book Name"><br>
-        <label for="Author_name"> Author Name </label>
-            <input type="text" name="Author_name" id="Author_name" placeholder="Author Name"><br>
-        <label for="Publication"> Publication </label>
-            <input type="text" name="Publication" id="Publication" placeholder="Publication"><br>
-        <label for="Edition"> Edition</label>
-            <input type="text" name="Edition" id="Edition" placeholder="Edition"><br>
-            <label for="ISBN"> ISBN Number</label>
-        <input type="text" name="ISBN" id="ISBN" placeholder="ISBN Number"><br>
-            <label for="Stock"> Stock</label>
-            <input type="text" name="Stock" id="Stock" placeholder="Stock"><br>
-             <?php insert_token(); ?>
-             <input type="Submit" name="Add_Book" value="Add Book"><br>
-    </form>
+   
     <form class="list-book" method="POST" action="config/book_fn.php">
         <label for="Book_name"> Book Name </label>
             <input type="text" name="Book_name" id="Book_name" placeholder="Book Name"><br>
@@ -37,9 +22,9 @@ include_once 'config/auth.php'; ?>
         <label for="ISBN"> ISBN Number</label>
             <input type="text" name="ISBN" id="ISBN" placeholder="ISBN Number"><br>
             
-             <?php insert_token(); ?>
+            <?php insert_token(); ?>
              <input type="Submit" name="List_Book" value="List Book"><br>
-    </form>
+    </form>  
     <?php  include "footer.php";?>
 </body>
 </html>
